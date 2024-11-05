876b794c387ad9b0e98ab4811d9def3109af599c
emxia18 <emxia18@gmail.com>
2024-11-04 17:45:36 -0800

    update api key

--- pupper_llm/pupper_llm/simple_scripts/simple_gpt_chat.py
+++ pupper_llm/pupper_llm/simple_scripts/simple_gpt_chat.py
@@ -6,7 +6,7 @@ GPT4_PROMPT = "I am a robotic dog. I have the following APIs to call: \
                'turn_left', 'turn_right', 'move', 'stop', 'bark'. \
                Please call one of the APIs based on the input. \
                For example, if you receive 'go_left', please output 'turn_left'. "
-API_KEY = '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'
+API_KEY = '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'
 
 client = OpenAI(api_key=API_KEY)
 
