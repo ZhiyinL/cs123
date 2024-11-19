efe5efbc535da7fddc8ad9e398b341c7bac17261
Zhiyin Lin <zhiyinl@stanford.edu>
2024-11-18 18:02:51 -0800

    draft code for main control

--- final_project/lab_7.py
+++ final_project/lab_7.py
@@ -13,11 +13,13 @@ IMAGE_WIDTH = 1400
 TIMEOUT = 2.0 #TODO threshold in timer_callback
 SEARCH_YAW_VEL = 1.0 #TODO searching constant
 TRACK_FORWARD_VEL = 0.5 #TODO tracking constant
+TRACK_HORIZONTAL_VEL = 0.1
 KP = 0.5 #TODO proportional gain for tracking
 
 class State(Enum):
     SEARCH = 0
-    TRACK = 1
+    ALIGN = 1
+    SHOOT = 2
 
 class StateMachineNode(Node):
     def __init__(self):
@@ -36,61 +38,75 @@ class StateMachineNode(Node):
             10
         )
 
+        self.annotation_subscription = self.create_subscription(
+            CompressedImage,
+            '/annotated_images', # todo: check path 
+            self.annotation_callback,
+            10
+        )
+
         self.timer = self.create_timer(0.1, self.timer_callback)
-        self.state = State.TRACK
+        self.state = State.SEARCH
 
         # TODO: Add your new member variables here
         self.kp = KP 
         self.latest_ts = 0
         self.target_x = 0
+        self.image = None # most up-to-date image
 
-    def detection_callback(self, msg):
-        """
-        Determine which of the HAILO detections is the most central detected object
-        """
-        
-        # Part 1.3, 1.5: Find the most central detection
-        if msg.detections:
-            normalized_x_positions = [
-                (detection.bbox.center.position.x - IMAGE_WIDTH / 2) / (IMAGE_WIDTH / 2)
-                for detection in msg.detections
-            ]
-            most_central_index = np.argmin(np.abs(normalized_x_positions))
-            self.target_x = normalized_x_positions[most_central_index]
+    def annotation_callback(self, msg):
+        image = msg.data # todo: check ros2 msg
+        self.image = image
 
-            # part 1.4 print x coordinate
-            for i, x_pos in enumerate(normalized_x_positions):
-                print(f"coordinate x_{i} normalized ", x_pos)
+    def check_search_to_align(self):
+        pass
 
-            # Update time of last detection
-            self.latest_ts = time.time()
+    def check_align_to_shoot(self):
+        pass
 
     def timer_callback(self):
         """
         Implement a timer callback that sets the moves through the state machine based on if the time since the last detection is above a threshold TIMEOUT
         """
-        dt = time.time() - self.latest_ts
-        if dt > TIMEOUT: # TODO: Part 3.2
-            self.state = State.SEARCH
-        else:
-            self.state = State.TRACK
-        
-        yaw_command = 0.0
-        forward_vel_command = 0.0
+        # Step 1. Image Processing (e.g. ball, net detection)
+        # find yellow
+        # find pink
 
+        # Step 2. Generate Motion
+        '''
+        states: SEARCH, ALIGN, SHOOT
+            SEARCH ---see ball + net--> ALIGN
+            ALIGN --in target position--> SHOOT
+        '''
+        # Step 2.1. State Transition
+        # switch search -> align if we see both ball and net, otherwise do nothing
+        if self.state == State.SEARCH and check_search_to_align():
+            self.state = State.ALIGN
+        # switch align -> shoot if we see 
+        if self.state == State.ALIGN and check_align_to_shoot():
+            self.state = State.SHOOT
+
+        # Step 2.2. State Execution
+        yaw_command = 0.0
+        horizontal_vel_command = 0.0
+        forward_vel_command = 0.0
         if self.state == State.SEARCH:
-            # TODO: Part 3.1
-            yaw_command = SEARCH_YAW_VEL * (-1 if self.target_x > 0 else 1)
-            
-        elif self.state == State.TRACK:
-            # TODO: Part 2 / 3.4
-            yaw_command = -self.kp*self.target_x # USE CONSTANT IN PT3
+            # rotate
+            yaw_command = SEARCH_YAW_VEL * (-1 if self.target_x > 0 else 1) 
+        elif self.state == State.ALIGN:
+            # move horizontally
+            # TODO: consider move away from ball on x axis and move towards the ball on y axis
+            yaw_command = 0.0 # -self.kp*self.target_x # USE CONSTANT IN PT3
+            horizontal_vel_command = TRACK_HORIZONTAL_VEL
+        elif self.state == State.SHOOT:
+            # move forward to shoot
             forward_vel_command = TRACK_FORWARD_VEL
 
         cmd = Twist()
         cmd.angular.z = yaw_command
         cmd.linear.x = forward_vel_command
-        self.command_publisher.publish(cmd)
+        cmd.linear.y = horizontal_vel_command
+        self.command_publisher.publish(cmd) 
 
 def main():
     rclpy.init()
